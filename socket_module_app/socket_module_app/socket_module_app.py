15e9393eae58d07ba5de0e26b4fa0bfdd512b016
Azaan-K <Azaan10141@gmail.com>
2024-07-27 00:17:04 -0600

    binded socket to addres variable

--- socket_module_app/socket_module_app/socket_module_app.py
+++ socket_module_app/socket_module_app/socket_module_app.py
@@ -2,4 +2,9 @@ import socket # module allows for access to the low-level networking interface
 import threading # module provides a way to create and manage multiple threads of execution within a single process
 
 PORT = 5050 # network port for running the server on
-SERVER = socket.gethostbyname(socket.gethostname()) # obtains the IP address of local host for the server 
+SERVER = socket.gethostbyname(socket.gethostname()) # obtains the IP address of local host for the server
+ADDRESS = (SERVER, PORT) # variable for binding socket to the ADDRESS
+print(f"PORT: {PORT}\nSERVER IP: {SERVER}") # logs port and server IP to console
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creating a socket using IPv4 and TCP 
+server.bind(ADDRESS)
