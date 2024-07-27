ce2a384c6ac3da6aafe96340e6c957a2dc7d9224
Azaan-K <Azaan10141@gmail.com>
2024-07-26 22:24:51 -0600

    imported socket and threading modules, create variables for the network port and server IP

--- socket_module_app/socket_module_app/socket_module_app.py
+++ socket_module_app/socket_module_app/socket_module_app.py
@@ -1,1 +1,5 @@
-print('Hello World')
+import socket # module allows for access to the low-level networking interface
+import threading # module provides a way to create and manage multiple threads of execution within a single process
+
+PORT = 5050 # network port for running the server on
+SERVER = socket.gethostbyname(socket.gethostname()) # obtains the IP address of local host for the server 
