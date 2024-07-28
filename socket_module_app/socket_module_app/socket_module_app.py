dfcbfeab14dd565e22db8c1ae4f3b8b9ea6e7047
Azaan-K <Azaan10141@gmail.com>
2024-07-27 19:42:50 -0600

    Added handle_client and start functions

--- socket_module_app/socket_module_app/socket_module_app.py
+++ socket_module_app/socket_module_app/socket_module_app.py
@@ -7,4 +7,20 @@ ADDRESS = (SERVER, PORT) # variable for binding socket to the ADDRESS
 print(f"PORT: {PORT}\nSERVER IP: {SERVER}") # logs port and server IP to console
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creating a socket using IPv4 and TCP 
-server.bind(ADDRESS)
+server.bind(ADDRESS) # binds socket to ADDRESS
+
+# function handles client and server connection
+def handle_client(connnection, address):
+    pass
+
+# function handles new connections to the server 
+def start():
+    server.listen()
+    while True:
+        connection, address =  server.accept()
+        thread = threading.Thread(target=handle_client, args=(connection, address))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+print("[SERVER] server is starting...")
+start()
